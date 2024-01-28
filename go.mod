c549d9b28707a4fc8878bd5d7fd98547b0367b41
budimanokky93 <budimanokky93@gmail.com>
2024-01-28 14:45:25 +0700

    added unit test login

--- go.mod
+++ go.mod
@@ -9,11 +9,13 @@ require (
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/lib/pq v1.10.9
 	github.com/oapi-codegen/runtime v1.1.1
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.18.0
 )
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.8 // indirect
 	github.com/google/uuid v1.5.0 // indirect
@@ -25,6 +27,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
--- handler/endpoints_test.go
+++ handler/endpoints_test.go
@@ -1,7 +1,71 @@
 package handler
 
-import "testing"
+import (
+	"github.com/SawitProRecruitment/UserService/repository"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
-func TestHello(t *testing.T) {
+func TestLoginEndpoint_Success(t *testing.T) {
+	// Create a new Echo instance
+	e := echo.New()
+	ctr := gomock.NewController(t)
+	defer ctr.Finish()
+	// Create a request with a valid payload
+	payload := `{"phone_number": "+6285797178075", "password": "Passw0rd!"}`
+	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(payload))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
 
+	// Create a mock server instance with an existing user
+	mockRepo := repository.NewMockRepositoryInterface(ctr)
+	// Set up expectations for GetUserByPhoneNumber
+	mockRepo.EXPECT().GetUserByPhoneNumber(gomock.Any(), gomock.Any()).Return(&repository.GetUserByPhoneNumberOutput{
+		Id:          1,
+		FullName:    "John Doe",
+		PhoneNumber: "+6285797178075",
+		Password:    "$2a$12$d9UJSAHKJeIQmA4BOA9SduFkxe6vcokolrRgJRS3dgfhyO0/NIFi6", // hashed password
+	}, nil)
+	s := &Server{
+		Repository: mockRepo,
+	}
+
+	// Call the Login endpoint
+	err := s.Login(c)
+
+	// Assertions
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
+}
+
+func TestLoginEndpoint_Failure(t *testing.T) {
+	// Create a new Echo instance
+	e := echo.New()
+	ctr := gomock.NewController(t)
+	defer ctr.Finish()
+	// Create a request with a valid payload
+	payload := `{"phone_number": "+6285797178075", "password": ""}`
+	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(payload))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Create a mock server instance with an existing user
+	mockRepo := repository.NewMockRepositoryInterface(ctr)
+	s := &Server{
+		Repository: mockRepo,
+	}
+
+	// Call the Login endpoint
+	err := s.Login(c)
+
+	// Assertions
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
 }
